46ab014b3bef0d61607138d48ffac37b273c097a
thaile-shin <shinichikudo9946@gmail.com>
2024-12-18 21:41:47 +0700

    Thực hành xây dựng Todo App #3

--- to-do-app/main.js
+++ to-do-app/main.js
@@ -19,6 +19,34 @@ const tasks = [
 // B3: đưa vào trong task-list
 const taskList = document.querySelector('#tasks-list');
 
+// B6: Làm thêm tính năng chỉnh sửa
+// B6.1: Bắt sự kiện onclick thẻ ul (tính chất của Bubble Event)
+taskList.onclick = function(e) {
+    // B6.4: Muốn có nội dung điền sẵn, ta lấy chỉ số trong mảng taske sau đó truy cập thuộc tính title trong phần tử. Vấn đề đặt ra là làm sao lấy đc chỉ số của phần tử trong mảng tasks => tham số thứ 2 của callbackFn là index.
+    // tự tạo 1 attribute (task-index)
+    // B6.5: lấy phần tử li cha từ phần tử con => dùng phương thức closest
+    const taskItem = e.target.closest('.task-item');
+    // B6.6: từ li tag => getAttribute sẽ lấy ra đc index + ép kiểu sang số.
+    const taskIndex = +taskItem.getAttribute('task-index');
+    const task = tasks[taskIndex];
+
+    console.log(task)
+    // B6.2 Kiểm tra 3 nút ng dùng bấm vào (e.target)
+    if(e.target.closest('.edit')) {
+        // B6.3: Bật prompt cho phép nhập công việc mới
+        const newTitle = prompt("Nhập công việc mới...", task.title);
+        // gán giá trị từ prompt cho task
+        task.title = newTitle;
+        render() // gọi lại để cập nhật giao diện
+    }
+    if(e.target.closest('.done')) {
+        console.log('Mark as done/ Undone')
+    }
+    if(e.target.closest('.delete')) {
+        console.log('Delete')
+    }
+}
+
 // B2: Sau khi đưa ra đc cấu trúc dữ liệu => render ra giao diện
 
 // B2.1: Chuyển Array sang chuỗi html, có cấu trúc giống file html => dùng Array.map() => trả về mảng có html có n phần tử, từ mảng trả về dùng Array.join() để trả về chuỗi
@@ -48,7 +76,7 @@ toDoForm.addEventListener("submit", function(e) {
     // tạo công việc mới theo định dạng có sẵn
     const newTasks = {
         title: value,
-        conpleted: false,
+        completed: false,
     }
 
     // đưa công việc mới vào bên trong danh sách công việc
@@ -66,8 +94,8 @@ toDoForm.addEventListener("submit", function(e) {
 // Vô hiệu hóa sự kiện mặc định khi nhấn chuôt: lấy ra #button, bắt sự kiện mousedown, dùng preventDefault();
 
 function render() {
-    const html = tasks.map(task => `
-        <li class="task-item ${task.completed ? 'completed' : ''}">
+    const html = tasks.map((task, index)=> `
+        <li class="task-item ${task.completed ? 'completed' : ''}" task-index="${index}">
             <span class="task-title">${task.title}</span>
             <div class="task-action">
                 <button class="task-btn edit">Edit</button>
